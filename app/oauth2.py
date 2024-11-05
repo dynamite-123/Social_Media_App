796827f5f99bab29fb698c177105cb87090d4e9f
dynamite-123 <108745890+dynamite-123@users.noreply.github.com>
2024-11-05 13:36:01 +0530

    bug fixes

--- app/oauth2.py
+++ app/oauth2.py
@@ -17,7 +17,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.now() + timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
@@ -27,7 +27,7 @@ def create_access_token(data: dict):
 def verify_access_token(token: str, credentials_exception):
 
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         id: str = payload.get("user_id")
 
         if id is None:
@@ -36,6 +36,8 @@ def verify_access_token(token: str, credentials_exception):
 
     except JWTError: 
         raise credentials_exception
+    
+    return token_data
     
 def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
